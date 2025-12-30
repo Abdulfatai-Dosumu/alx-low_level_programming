5f60613b764054316fd827dd16e67194c92cd1ec
Abdulfatai Dosumu <abdulfataidosumu@DESKTOP-8U6V1UR.localdomain>
2025-12-30 22:22:54 +0100

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - This function adds two integers and returns the result.
+* @a: first integer
+* @b: second integer
+*
+* Return: @c - the result of the addition.
+*/
+int add(int a, int b)
+{
+  int c;
+
+  c = a + b;
+  return (c);
+}
