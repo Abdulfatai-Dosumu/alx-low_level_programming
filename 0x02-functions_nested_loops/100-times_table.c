a20b980f19a73715dbb580d62093ef8c7a493226
Abdulfatai Dosumu <abdulfataidosumu@DESKTOP-8U6V1UR.localdomain>
2025-12-30 21:50:45 +0100

    Times-table done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+ /**
+* print_times_table - prints the n times table, starting with 0.
+* @n: an integer.
+*
+* Return: none.
+*/
+void print_times_table(int n)
+{
+  int i,j,k;
+  if (n <= 15 && n >= 0)
+  {
+    for (i = 0; i <= n; i++)
+    {
+      for (j = 0; j <= n; j++)
+      {
+        k = i * j;
+
+        if (k == 0 && j == 0)
+        {
+          _putchar(k + '0');
+        }
+        else if ((k > 0 && k < 10) || (i == 0 && j > 0))
+        {
+          _putchar(' ');
+          _putchar(' ');
+          _putchar(k + '0');
+        }
+        else if (k > 9 && k < 100)
+        {
+          _putchar(' ');
+          _putchar((k / 10) + '0');
+          _putchar((k % 10) + '0');
+        }
+        else if (k > 99)
+        {
+          _putchar((k / 100) + '0');
+          _putchar(((k /100) % 10) + '0');
+          _putchar((k % 10) + '0');
+        }
+
+        if (j != n)
+        {
+          _putchar(',');
+          _putchar(' ');
+        }
+      }
+      _putchar('\n');
+    }
+  }
+}
